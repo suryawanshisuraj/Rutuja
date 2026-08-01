cdc1cee1c8492c2afa4d43184653791c425a009e
prateek541 <prateek541@users.noreply.github.com>
2026-08-01 13:40:36 +0530

    Optimize Vercel serverless function and build config

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
-import app from '../server/index.js';
+import express from 'express';
+import cors from 'cors';
+import apiRoutes from '../server/routes/api.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// API routes
+app.use('/api', apiRoutes);
+app.use('/', apiRoutes);
 
 export default app;
